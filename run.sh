a8703f316cf45377558d5a5e9c410ed7b8e6c8d4
Arpan Pandey <arpan@hackersreboot.tech>
2022-09-11 18:47:28 +0530

    Minor checks

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@
 python certificates_backend/deleter.py &
   
 # Start the second process
-gunicorn certificates_backend.__main__:app --workers 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:80 &
+#gunicorn certificates_backend.__main__:app --workers 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:80
+uvicorn certificates_backend.__main__:app  --host 0.0.0.0 --port 8000 &
   
 # Wait for any process to exit
 wait -n
